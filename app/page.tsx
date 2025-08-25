68253b52150f6188cde1392c70552fabf87fc19e
samitkapoor <samitkapoor77@gmail.com>
2025-08-25 10:44:51 +0530

    Implement initial project structure and setup

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="bg-background h-screen w-screen flex flex-col items-center">
+      <h1 className="flex items-center gap-2 text-black text-3xl mt-20">
+        <div className="flex items-end">
+          <div className="h-9 w-9 rounded-3xl flex items-center justify-center">
+            <img src="/peerlist.svg" className="w-full h-full" />
+          </div>
+          <p className="text-black/80 font-bold">eerlist&apos;s</p>
+        </div>
+        Interaction Design Challenge
+      </h1>
+    </div>
+  );
+}
