7ec49ebb9c9ea70d86f25a5ad8299265b435dd5b
samitkapoor <samitkapoor77@gmail.com>
2025-08-26 15:32:27 +0530

    Add disabled state to OTPInput component; prevent user interaction when input state is 'success'.

--- components/otp-input.tsx
+++ components/otp-input.tsx
@@ -221,6 +221,7 @@ const OTPInputBox = ({
         onBlur={onBlur}
         placeholder="0"
         className="border-none outline-none w-9 h-10 text-center placeholder:text-black/15 caret-transparent"
+        disabled={state === 'success'}
       ></motion.input>
     </motion.div>
   );
