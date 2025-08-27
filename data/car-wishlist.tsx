811c75b666ae6639482f86456b13dd2e61f69a8e
samitkapoor <samitkapoor77@gmail.com>
2025-08-27 15:46:45 +0530

    Update car-wishlist data to replace image formats with WebP for improved loading performance and visual quality.

--- data/car-wishlist.tsx
+++ data/car-wishlist.tsx
@@ -4,8 +4,8 @@ export const CAR_WISHLIST = [
   //   boldText: <p className="text-black opacity-10 italic text-[175px]">GT3 RS</p>,
   //   boldTextPosition: 'top-2/5 -translate-y-1/2 left-1/2 -translate-x-1/2 leading-[150px]',
   //   variant: 'porsche',
-  //   image: '/911gt3rsmodel.png',
-  //   backgroundImage: '/911gt3rs.jpg',
+  //   image: '/911gt3rsmodel.webp',
+  //   backgroundImage: '/911gt3rs.webp',
   //   price: '₹3,50,00,000',
   //   priceClassName: 'text-white font-bold text-[40px] absolute right-10 bottom-10',
   //   description: (
@@ -33,7 +33,7 @@ export const CAR_WISHLIST = [
   //     duration: 0.3,
   //     ease: 'easeInOut'
   //   },
-  //   image: '/porsche918model.png',
+  //   image: '/porsche918model.webp',
   //   backgroundImage: '/porsche918.webp',
   //   description: (
   //     <p>
@@ -53,8 +53,8 @@ export const CAR_WISHLIST = [
       duration: 0.3,
       ease: 'easeInOut'
     },
-    image: '/911model.png',
-    backgroundImage: '/911.jpg',
+    image: '/911model.webp',
+    backgroundImage: '/911.webp',
     description: (
       <p>It's a little too small to get laid in, but you get laid the minute you get out</p>
     ),
@@ -88,8 +88,8 @@ export const CAR_WISHLIST = [
       duration: 0.3,
       ease: 'easeInOut'
     },
-    image: '/bmwm2model.png',
-    backgroundImage: '/bmwm2.jpg',
+    image: '/bmwm2model.webp',
+    backgroundImage: '/bmwm2.webp',
     variant: 'trippy-reverse',
     description: (
       <p
@@ -128,12 +128,12 @@ export const CAR_WISHLIST = [
     ),
     boldTextPosition: 'top-0 right-0 leading-[250px] text-white',
     variant: 'trippy',
-    image: '/r34model.png',
+    image: '/r34model.webp',
     imageTransition: {
       duration: 0.3,
       ease: 'easeInOut'
     },
-    backgroundImage: '/r34.jpg',
+    backgroundImage: '/r34.webp',
     price: <p className="text-white text-[125px] font-black">スカイライン</p>,
     priceClassName: 'absolute bottom-0 leading-[125px] left-0 text-white',
     description: "If one day speed kills me, don't cry because I was smiling.",
